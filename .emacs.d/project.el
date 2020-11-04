b09bea75135d09781d42e5c03aa740f46fec0726
Olav Fosse <mail@olavfosse.no>
2020-11-04 17:26:48 +0100

    Switch to projectile

--- .emacs.d/project.el
+++ .emacs.d/project.el
@@ -1,13 +1,19 @@
-;; project.el – project management sans projectile
+;; project.el – project management ~sans~ with projectile
 (defun olav-open-project ()
   (interactive)
   (cd (completing-read "Project: " (append '("/home/olav/.emacs.d") (directory-files "~/Projects" t)) nil))
-  (call-interactively 'find-file)
-  )
+  (call-interactively 'find-file))
 
-(use-package project
-  :bind (("C-c j f" . project-or-external-find-file)
-	 ("C-c j d" . project-or-exernal-find-regexp)
-	 ("C-c j r" . project-query-replace-regexp)
-	 ("C-c j s" . project-search)
-	 ("C-c j o" . olav-open-project)))
+(use-package projectile
+  :init
+  (setq projectile-project-search-path '("~/Projects"))
+  (setq projectile-completion-system 'default) ;; Use selectrum completion
+  (setq projectile-indexing-method 'hybrid)
+  (setq projectile-sort-order 'recently-active) ;; Sort files by recently active buffer and then recenently opened files
+  (setq projectile-current-project-on-switch 'keep) ;; Include current project in project list
+  (setq projectile-switch-project-action 'projectile-vc) ;; Open magit when switching to a project
+  :config
+  (projectile-mode +1)
+  :bind (("C-c j" . projectile-command-map)
+	 (:map projectile-command-map
+	       ("A" . projectile-add-known-project))))
