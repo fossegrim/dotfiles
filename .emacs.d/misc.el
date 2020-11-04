7ecd90265bcc231bcaeabd72daf98cfe944030bd
Olav Fosse <mail@olavfosse.no>
2020-11-04 14:45:42 +0100

    Add delete-trailing-white-space key binding

--- .emacs.d/misc.el
+++ .emacs.d/misc.el
@@ -1,7 +1,10 @@
 ;; misc.el – miscelaneus configurations
 (fset 'yes-or-no-p 'y-or-n-p)
 (load-theme 'wombat)
+;; a comfy isearch replacement, unfortunately it doesn't do search-and-replace
 (use-package ctrlf
   :config (ctrlf-mode +1))
 (use-package which-key
   :config (which-key-mode))
+;; remove all trailing whitespace and newlines, handy before commiting.
+(global-set-key (kbd "C-c d") 'delete-trailing-whitespace)
