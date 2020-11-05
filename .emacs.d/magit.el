6cb92dc17c30dca676c1ca0bd18e0fc312d6fe51
Olav Fosse <mail@olavfosse.no>
2020-11-06 00:48:15 +0100

    Further configure magit

--- .emacs.d/magit.el
+++ .emacs.d/magit.el
@@ -1,5 +1,8 @@
 ;; magit.el – configurations for the magit git interface
 (use-package magit
   :init
+  (setq magit-clone-default-directory "~/Projects/")
+  (setq magit-clone-set-remote.pushDefault t) ; set it without prompting
   (setq magit-save-repository-buffers nil) ;; don't prompt to save files when running magit commands
-  :bind (("C-c g" . magit-status)))
+  :bind
+  ("C-c q" . magit-clone))
