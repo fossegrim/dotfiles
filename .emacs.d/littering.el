9feabbcabf992ce95f4808c70fc63a53c1aa4877
Olav Fosse <mail@olavfosse.no>
2020-11-06 02:05:08 +0100

    Set auto-save-default to nil

--- .emacs.d/littering.el
+++ .emacs.d/littering.el
@@ -1,3 +1,4 @@
 ;; littering.el – configurations to prevent the creation of garbage files
+(setq auto-save-default nil)
 (setq create-lockfiles nil)  ; stop creating .# files
 (setq make-backup-files nil) ; stop creating ~ files
