7aab4e797d90cc50bbebbe42f4bfa90a99996342
Olav Fosse <mail@olavfosse.no>
2020-11-09 00:01:50 +0100

    Further configure erc

--- .emacs.d/irc.el
+++ .emacs.d/irc.el
@@ -1,11 +1,25 @@
 ;; irc.el – erc configuration
 
-;; C-c C-b for switching buffers does not work with persp-mode, also see
 (use-package erc
   :init
+  (load "~/.emacs.d/erc-auth.el")
   (setq erc-nick "fossegrim")
+  (setq erc-server "irc.freenode.net")
+  (setq erc-port 6667)
+  (setq erc-part-reason "email me at {mail,post,spam}@olavfosse.no")
+  (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#erc")))
   :config
   (defun olav-erc ()
     (interactive)
     (persp-switch "irc")
-    (call-interactively 'erc)))
+    (erc :server "irc.freenode.net"
+	 :port "6667"
+         :nick "fossegrim"
+	 :password olav-freenode-password ; nice try, it's not vc'ed
+	 :full-name "Olav Fosse"))
+  :bind (("C-c i i" . 'olav-erc)
+	 (:map erc-mode-map
+	       ("RET" . nil)
+	       ("C-c <C-return>" . 'erc-send-current-line)
+	       ("C-c C-b" . nil) 	; erc-switch-to-buffer does not work with persp-mode and is therefore unbound.
+	       )))
