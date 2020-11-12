1fe0c05477686525d4bfad657e046b6cb419def1
Olav Fosse <mail@olavfosse.no>
2020-11-12 14:36:13 +0100

    update irc config

--- .emacs.d/irc.el
+++ .emacs.d/irc.el
@@ -4,44 +4,50 @@
   :init
   (load "~/.emacs.d/erc-auth.el")
   (setq erc-autojoin-channels-alist
-	'(("freenode.net"
-	   ;; friends
-	   "#distrotube"
-	   "#tripcode"
-	   ;; emacs
-	   "#emacs"
-	   "#erc"
-	   "#org-mode"
-	   ;; web
-	   "#web"
-	   "#html"
-	   "#css"
-	   "##javascript"
-	   "##typescript"
-	   "#reactjs"
-	   "#graphql"
-	   "#node.js"
-	   "#socket.io"
-	   ;; unix/cs/systems
-	   "#openbsd"
-	   "##c"
-	   "##c++"
-	   "##sqlite"
-	   "##algorithms"
-	   )))
+   	'(("freenode.net"
+   	   ;; friends
+   	   "#distrotube"
+   	   "#tripcode"
+  ;; 	   ;; emacs
+  ;; 	   "#emacs"
+  ;; 	   "#erc"
+  ;; 	   "#org-mode"
+  ;; 	   ;; web
+  ;; 	   "#web"
+  ;; 	   "#html"
+  ;; 	   "#css"
+  ;; 	   "##javascript"
+  ;; 	   "##typescript"
+  ;; 	   "#reactjs"
+  ;; 	   "#graphql"
+  ;; 	   "#node.js"
+  ;; 	   "#socket.io"
+  ;; 	   ;; unix/cs/systems
+  ;; 	   "#openbsd"
+  ;; 	   "##c"
+  ;; 	   "##c++"
+  ;; 	   "##sqlite"
+  ;; 	   "##algorithms"
+  	   )))
   :config
-  ;; I use IRC for ephemeral communications and therefore do not utilize notifications or a bouncer.
-  ;; Therefore I refer people to my email in case they need to contact me when I am not present.
+  ;; I use IRC for ephemeral communications only and therefore do not utilize notifications or a bouncer.
+  ;; I refer people to my email in case they need to contact me when I am not present.
   (defun erc-quit/part-reason-default ()
     "email me at {mail,post,spam}@olavfosse.no")
   (defun olav-erc ()
+    "If erc pespective exists, switch to it, otherwise create it and open erc there.
+
+NB: perspectives are per frame.
+"
     (interactive)
-    (persp-switch "irc")
-    (erc :server "irc.freenode.net"
-	 :port "6667"
-         :nick "fossegrim"
-	 :password olav-freenode-password ; nice try, it's not vc'ed
-	 :full-name "Olav Fosse"))
+    (when (not (-contains-p (hash-table-keys (perspectives-hash)) "erc"))
+      (erc :server "irc.freenode.net"
+	   :port "6667"
+           :nick "fossegrim"
+	   :password olav-freenode-password ; nice try, it's not vc'ed
+	   :full-name "Olav Fosse"))
+    (persp-switch "erc"))
+
   :bind (("C-c i i" . 'olav-erc)
 	 (:map erc-mode-map
 	       ("RET" . nil)
