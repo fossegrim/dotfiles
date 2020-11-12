ff9678f3fc08d61d26da59191b633e4c37d7b70c
Olav Fosse <mail@olavfosse.no>
2020-11-12 14:31:44 +0100

    Update rss.el

--- .emacs.d/rss.el
+++ .emacs.d/rss.el
@@ -2,6 +2,11 @@
 (use-package elfeed
   :init
   (setq elfeed-feeds
-	'(("https://fossegr.im/feed.xml" blog)
-	  ("https://sachachua.com/blog/feed/" blog)
-	  ("https://protesilaos.com/codelog.xml" blog))))
+	'("https://fossegr.im/feed.xml"
+	  "https://protesilaos.com/codelog.xml"))
+  :config
+  (defun olav-elfeed ()
+    (interactive)
+    (persp-switch "elfeed")
+    (elfeed))
+  :bind ("C-c l l" . olav-elfeed))
