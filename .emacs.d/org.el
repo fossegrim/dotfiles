1626f9b5d3ac3060c89e1d986c0f5a36521d9594
Olav Fosse <mail@olavfosse.no>
2020-12-02 23:56:20 +0100

    Update org.el

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -1,8 +1,8 @@
 ;; org.el – org-mode configurations
-;; Make it possible to insert code blocks with <s
 (use-package org
+  :init
+  (setq org-agenda-files '("~/Projects/org/todo.org"))
   :config
   (require 'org-tempo)
   :bind
-  ("C-c o l" . org-store-link)
-  )
+  ("C-c o l" . org-store-link))
