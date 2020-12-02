9dd98065926bc756938d25d4304bf5eba15b69c1
Olav Fosse <mail@olavfosse.no>
2020-12-02 23:57:17 +0100

    Stop displaying \ at the "end" of truncated lines

--- .emacs.d/lines.el
+++ .emacs.d/lines.el
@@ -2,3 +2,5 @@
 ;; use relative line numbers
 (add-hook 'find-file-hook 'display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
+;; Do not display backslash(\) at to illustrate that a truncated line. Instead rely on line numbers to tell.
+(set-display-table-slot standard-display-table 'wrap ?\ )
