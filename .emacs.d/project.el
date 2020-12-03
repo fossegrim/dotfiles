6d870082acbfc08c2f7de530bd677132fde6fb31
Olav Fosse <mail@olavfosse.no>
2020-12-03 01:38:02 +0100

    Add olav-projectile-external-terminal

--- .emacs.d/project.el
+++ .emacs.d/project.el
@@ -7,9 +7,15 @@
   (setq projectile-sort-order 'recently-active) ;; Sort files by recently active buffer and then recenently opened files
   (setq projectile-use-git-grep t) ; only grep vced files in git projects
   (setq projectile-switch-project-action 'projectile-dired)
+  (defun olav-projectile-external-terminal ()
+    (interactive)
+    (if (string-equal system-type "darwin")
+	 (shell-command (concat "open -na Terminal.app " (projectile-project-root))
+       (message "no external terminal configured for non-macos system"))))
   :config
   (projectile-mode +1)
   :bind (("C-c j" . projectile-command-map)
 	 (:map projectile-command-map
 	       ("s" . projectile-grep) ;; just use grep
 	       ("A" . projectile-add-known-project))))
+	       ("x T" . olav-projectile-external-terminal))))
