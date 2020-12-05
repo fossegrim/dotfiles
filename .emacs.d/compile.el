9dcc6eec003b886932d7ebfdf0889d524a0e59b5
Olav Fosse <mail@olavfosse.no>
2020-12-05 17:52:10 +0100

    Change (re)compile keybindings

--- .emacs.d/compile.el
+++ .emacs.d/compile.el
@@ -1,4 +1,4 @@
 ;; compile.el – M-x [re]compile configurations
 (setq compile-command "make")
-(global-set-key (kbd "C-c c") 'compile)
-(global-set-key (kbd "C-c r") 'recompile)
+(global-set-key (kbd "C-c C") 'compile)
+(global-set-key (kbd "C-c c") 'recompile)
