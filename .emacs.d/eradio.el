538f430359294fbc14c0a8411b47b6d99dde1c63
Olav Fosse <mail@olavfosse.no>
2020-12-06 22:27:08 +0100

    Set eradio-current-channel

--- .emacs.d/eradio.el
+++ .emacs.d/eradio.el
@@ -16,5 +16,6 @@
 			  ("4059 - 0xfdb.fm"  . "https://music.taro.0xfdb.xyz:8443/stream.flac")
 			  ("classical - sonatica.fm" . "https://orion.shoutca.st/tunein/sonatica.pls")
 			  ("l's theme (10 hours) - death note" . "https://www.youtube.com/watch?v=bAr1a6zux_8"))
+	eradio-current-channel "https://somafm.com/defcon256.pls")
   :bind (("C-c R" . eradio-play)
 	 ("C-c r" . eradio-toggle)))
