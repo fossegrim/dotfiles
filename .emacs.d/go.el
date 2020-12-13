1e260c6f6132fd197f2c69ba90e3f64fb7608317
Olav Fosse <mail@olavfosse.no>
2020-12-13 19:09:53 +0100

    Add godef installation instructions

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,6 +1,9 @@
 ;; go.el - go language configuration
 (use-package go-mode
   :init
+  ;; godef must be installed and in PATH
+  ;; ta install godef to $HOME/go/bin ru
+  ;; $ go get github.com/rogpeppe/godef
   ;; goimports must be installed and in PATH
   ;; to install goimports to $HOME/go/bin run
   ;; $ go get golang.org/x/tools/cmd/goimports
