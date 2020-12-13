2c37e6c6247efbe91a81551ce9eb1d088da77b7c
Olav Fosse <mail@olavfosse.no>
2020-12-13 18:14:27 +0100

    Add godoc keybindings

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -6,4 +6,9 @@
   ;; $ go get golang.org/x/tools/cmd/goimports
   (setq gofmt-command "goimports")
   :config
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  :bind
+  (:map go-mode-map
+	("C-c C-S-d" . 'godoc))
+  (:map godoc-mode-map
+	("C-c C-S-d" . 'godoc)))
