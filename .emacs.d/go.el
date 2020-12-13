c3411898a0caca5e5a90f1505265ec5e03721793
Olav Fosse <mail@olavfosse.no>
2020-12-13 11:20:05 +0100

    Add goimports installation instructions

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,8 +1,9 @@
 ;; go.el - go language configuration
 (use-package go-mode
   :init
-  ;; goimports must be in PATH
-  ;; goimports is in $HOME/go/bin
+  ;; goimports must be installed and in PATH
+  ;; to install goimports to $HOME/go/bin run
+  ;; $ go get golang.org/x/tools/cmd/goimports
   (setq gofmt-command "goimports")
   :config
   (add-hook 'before-save-hook 'gofmt-before-save))
