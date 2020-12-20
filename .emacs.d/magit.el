5e2cb3fa3b9917ee4e1963cf87e4e0352ae162be
Olav Fosse <mail@olavfosse.no>
2020-12-20 01:26:03 +0100

    Set magit-clone-default-directory to "~/vc/"

--- .emacs.d/magit.el
+++ .emacs.d/magit.el
@@ -1,7 +1,7 @@
 ;; magit.el – configurations for the magit git interface
 (use-package magit
   :init
-  (setq magit-clone-default-directory "~/Projects/")
+  (setq magit-clone-default-directory "~/vc/")
   (setq magit-clone-set-remote.pushDefault t) ; set it without prompting
   (setq magit-save-repository-buffers nil) ;; don't prompt to save files when running magit commands
   :config
