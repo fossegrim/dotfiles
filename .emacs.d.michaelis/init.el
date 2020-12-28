f29c02350804285ddd928dc50ced5adcd9e37cfe
Olav Fosse <mail@olavfosse.no>
2020-12-28 05:06:24 +0100

    Add .emacs.d.michaelis
    This is a fresh minimalist emacs configuration for use on my desktop
    computer.

---
+++ .emacs.d.michaelis/init.el
@@ -0,0 +1,16 @@
+(load-file "~/.emacs.d/melpa.el")
+(load-file "~/.emacs.d/go-mode.el")
+(load-file "~/.emacs.d/magit.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (magit go-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d.michaelis/magit.el
@@ -0,0 +1,1 @@
+(when (not (package-installed-p 'magit)) (package-install 'magit))
